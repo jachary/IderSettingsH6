5534f540ae42091416653c384a2972f92b272082
guoliang.wgl <guoliang.wgl@alibaba-inc.com>
2018-03-17 15:24:46 +0800

    add hdmi mode change funs

--- app/src/main/java/com/rk_itvui/settings/ScreensSettings.java
+++ app/src/main/java/com/rk_itvui/settings/ScreensSettings.java
@@ -30,11 +30,15 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.hardware.display.DisplayManager;
 import android.os.Bundle;
+import android.os.DisplayOutputManager;
 import android.os.Handler;
 import android.os.Message;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.IWindowManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -47,13 +51,22 @@ public  class ScreensSettings extends FullScreenPreferenceActivity implements On
 //	private ScreenSetting   displaysetting_local;
 
 	private Context mContext = ScreensSettings.this;
+	private String TAG = "Settings2" ;
+	private String SubTAG = "--ScreensSettings:" ;
+	private DisplayOutputManager mDisplayManager;
 	
 	private String setiface , setmode;
 	private String iface , vediomode , size ;
 	private String resolution_mode = null;
 	private int HDMI_TV = -1;
+	private int format = 0;
+	private String oldValue;
+	private boolean isSameMode;
+	private boolean isSupport = false;
+	private boolean dialogTipsFlag;
 	String[] mode_entries ;
-	int[] mode_value ;
+	String[] mode_value ;
+	//int[] mode_value ;
 	HashMap<String, Object> map_dspscaleItem = new HashMap<String, Object>();
 	HashMap<String, Object> map_dspmodeItem = new HashMap<String, Object>();
 	HashMap<String, Object> map_dspifaceiItem = new HashMap<String, Object>();
@@ -117,6 +130,8 @@ public  class ScreensSettings extends FullScreenPreferenceActivity implements On
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.screen_settings);
 		addListView();
+		mDisplayManager = (DisplayOutputManager)this.getSystemService(
+				Context.DISPLAYOUTPUT_SERVICE);
 //		displaysetting_local.getCurrentmode();
 		 res =getResources();
 		 pm = PreferenceManager.getInstance(this);
@@ -203,32 +218,26 @@ public void onItemClick(AdapterView<?> arg0, View view, int index, long arg3) {
 	switch (index) {
 	case 0:
 		Log.d("ScreenSettings", "=====================================displayscale");
-//		Intent intentscale = new Intent(ScreensSettings.this, DisplayPercentPreference.class);
-//		startIntentWithTranlete(ScreensSettings.this,intentscale);
-////		Intent intentscale1 = new Intent();
-////		intentscale1.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$DisplaySettingsActivity"));
-////        startActivity(intentscale1);
 		Intent intent = new Intent();
-
 		intent.setComponent(new ComponentName(this.getPackageName(), "com.rk_itvui.settings.dialog.DisplaySize"));
 		startActivity(intent);
 		break;
 	case 1:
-		 Log.d("ScreenSettings", "=====================================onclick1");
-			if(isHdmiMode()) {
-			final int hdmi_4k_ban = SystemProperties.getInt("persist.sys.hdmi_4k_ban", 0);
-			if(hdmi_4k_ban == 0){
-				mode_entries = res.getStringArray(R.array.hdmi_output_mode_entries);
-				mode_value = res.getIntArray(R.array.hdmi_output_mode_values);
-				}else{
-					mode_entries = res.getStringArray(R.array.hdmi_output_mode_without_4k_entries);
-					mode_value = res.getIntArray(R.array.hdmi_output_mode_without_4k_values);	
-				}
-			}else{
-				mode_entries = res.getStringArray(R.array.cvbs_output_mode_entries);
-				mode_value = res.getIntArray(R.array.cvbs_output_mode_values);
-			}
-			showDisplayDialog();
+		Log.d("ScreenSettings", "=====================================onclick1");
+			//if(isHdmiMode()) {
+			//final int hdmi_4k_ban = SystemProperties.getInt("persist.sys.hdmi_4k_ban", 0);
+			//if(hdmi_4k_ban == 0){
+		mode_entries = res.getStringArray(R.array.hdmi_output_mode_entries);
+		mode_value = res.getStringArray(R.array.hdmi_output_mode_values);
+//				}else{
+//					mode_entries = res.getStringArray(R.array.hdmi_output_mode_without_4k_entries);
+//					mode_value = res.getIntArray(R.array.hdmi_output_mode_without_4k_values);
+//				}
+//			}else{
+//				mode_entries = res.getStringArray(R.array.cvbs_output_mode_entries);
+//				mode_value = res.getIntArray(R.array.cvbs_output_mode_values);
+//			}
+		showDisplayDialog();
 //			Intent intentscale2 = new Intent();
 //			intentscale2.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$DisplaySettingsActivity"));
 //	        startActivity(intentscale2);
@@ -273,31 +282,31 @@ public void onItemClick(AdapterView<?> arg0, View view, int index, long arg3) {
 
 
 
-boolean plugged = true;//hdmi or cvbs
-private boolean isHdmiMode(){
-	final String filename = "/sys/class/switch/hdmi/state";
-        FileReader reader = null;
-        try {
-            reader = new FileReader(filename);
-            char[] buf = new char[15];
-            int n = reader.read(buf);
-            if (n > 1) {
-                plugged = 0 != Integer.parseInt(new String(buf, 0, n-1));
-            }
-        } catch (IOException ex) {
-			return false;
-        } catch (NumberFormatException ex) {
-			return false;
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException ex) {
-                }
-            }
-        }
-	return plugged;
-}
+//boolean plugged = true;//hdmi or cvbs
+//private boolean isHdmiMode(){
+//	final String filename = "/sys/class/switch/hdmi/state";
+//        FileReader reader = null;
+//        try {
+//            reader = new FileReader(filename);
+//            char[] buf = new char[15];
+//            int n = reader.read(buf);
+//            if (n > 1) {
+//                plugged = 0 != Integer.parseInt(new String(buf, 0, n-1));
+//            }
+//        } catch (IOException ex) {
+//			return false;
+//        } catch (NumberFormatException ex) {
+//			return false;
+//        } finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException ex) {
+//                }
+//            }
+//        }
+//	return plugged;
+//}
 public Dialog mOutputInterfaceDialog = null;
 
 
@@ -313,34 +322,147 @@ public int setcurrent(String currentdate){
 }
 int i = 2;
 public void showDisplayDialog(){
+	int currentIndex = 0;
+	format  = mDisplayManager.getDisplayOutput(android.view.Display.TYPE_BUILT_IN);
+	oldValue = Integer.toHexString(format);
+	for(int i = 0;i<mode_value.length;i++){
+
+		if((mode_value[i]+"").equals(oldValue)){
+			currentIndex = i;
+			break;
+		}
+	}
 	mOutputInterfaceDialog = new AlertDialog.Builder(mContext)
 	.setTitle(R.string.screen_interface)
-	.setSingleChoiceItems(mode_entries, pm.getBootCount(), new OnClickListener() {
+	.setSingleChoiceItems(mode_entries, currentIndex, new OnClickListener() {
 		@Override
 		public void onClick(DialogInterface dialog, final int which) {
-			new Handler().postDelayed(new Runnable() {
-				@Override
-				public void run() {
-					int mode = mode_value[which];
-					i = which;					
-					Intent intent = new Intent("com.zxy.display");
-					intent.putExtra("display", mode);
-					intent.putExtra("displayvalue",mode_entries[which]);
-					sendBroadcast(intent);
-				}
-			}, 500);
-			dialog.dismiss();
+//			new Handler().postDelayed(new Runnable() {
+//				@Override
+//				public void run() {
+//					int mode = mode_value[which];
+//					i = which;
+//					Intent intent = new Intent("com.zxy.display");
+//					intent.putExtra("display", mode);
+//					intent.putExtra("displayvalue",mode_entries[which]);
+//					sendBroadcast(intent);
+//				}
+//			}, 500);
+			//dialog.dismiss();
+			Log.d(TAG,SubTAG+"click index is :" +which);
+			//switchDispFormat(mode_entries[which]);
+
+			Log.d(TAG,SubTAG + "oldValue is :" + oldValue +"checked value is :" + mode_value[which]);
+			isSameMode = oldValue.equals(mode_value[which]);
+			if(isSameMode)
+				return ;
+			switchDispFormat(mode_value[which]+"");
 		}
 	})
 	.setOnCancelListener(new DialogInterface.OnCancelListener() {
 		@Override
 		public void onCancel(DialogInterface dialog) {
 			// TODO Auto-generated method stub
-			
+		Log.d(TAG,SubTAG +" cancel dialog");
 			dialog.dismiss();
 		}
 	}).create();
 	mOutputInterfaceDialog.show();
 }
 
+
+	private void switchDispFormat(String value) {
+		Log.d(TAG,SubTAG +"switchDispFormat value : " + value);
+		try {
+			format = Integer.parseInt(value, 16);
+			int dispformat = mDisplayManager.getDisplayModeFromFormat(format);
+			Log.d(TAG,SubTAG +"disformat is :"+ dispformat);
+			int mCurType = mDisplayManager.getDisplayOutputType(android.view.Display.TYPE_BUILT_IN);
+			IWindowManager wm = IWindowManager.Stub.asInterface(ServiceManager.checkService(
+					Context.WINDOW_SERVICE));
+			int setDispOutputOk = 0;
+			int w = 0;
+			int h = 0;
+			int density = 0;
+
+			isSupport = true;
+			if(isSupport){
+				Log.w(TAG, "dm set output format:"+format);
+				setDispOutputOk = mDisplayManager.setDisplayOutput(android.view.Display.TYPE_BUILT_IN, format);
+				Log.w(TAG, "setDisplayOutput return "+setDispOutputOk);
+				if(setDispOutputOk == -1 && dialogTipsFlag){
+					dialogTipsFlag = !dialogTipsFlag;
+					Dialog alertDialog = new AlertDialog.Builder(this)
+							.setTitle(this.getResources().getString(R.string.display_support_type_title))
+							.setMessage(this.getResources().getString(R.string.display_support_type))
+							.setPositiveButton(
+									//this.getResources().getString(""),
+									"确定",
+									new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(DialogInterface dialog,
+															int which) {
+											switchDispFormat(oldValue);
+											return ;
+										}
+									}).create();
+					alertDialog.show();
+				}
+//				mOutputMode.setValue(value);
+//				if(mHdmiOutputModePreference!=null)
+//					mHdmiOutputModePreference.setValue(value);
+				if(0xFF == setDispOutputOk) {
+					switch(dispformat) {
+						case DisplayOutputManager.DISPLAY_TVFORMAT_3840_2160P_30HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_3840_2160P_25HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_3840_2160P_24HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_1080P_50HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_1080P_60HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_1080I_60HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_1080I_50HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_1080P_24HZ:
+							w = 1920;
+							h = 1080;
+							density = 240;
+							break;
+						case DisplayOutputManager.DISPLAY_TVFORMAT_720P_50HZ:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_720P_60HZ:
+							w = 1280;
+							h = 720;
+							density = 160;
+							break;
+						case DisplayOutputManager.DISPLAY_TVFORMAT_576P:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_576I:
+							w = 720;
+							h = 576;
+							density = 128;
+							break;
+						case DisplayOutputManager.DISPLAY_TVFORMAT_480P:
+						case DisplayOutputManager.DISPLAY_TVFORMAT_480I:
+							w = 720;
+							h = 480;
+							density = 106;
+							break;
+						default:
+							setDispOutputOk = 1;
+					}
+				}
+				if(0xFF == setDispOutputOk) {
+					if (wm == null) {
+						Log.e(TAG,"wm is null!");
+					}
+					try{
+						wm.setForcedDisplaySize(android.view.Display.DEFAULT_DISPLAY, w, h);
+						wm.setForcedDisplayDensity(android.view.Display.DEFAULT_DISPLAY, density);
+					}catch(RemoteException e){
+						Log.d(TAG,"call setForcedDisplaySize error");
+					}
+				}
+			}else {
+				//Toast.makeText(getActivity(), com.android.settings.R.string.display_mode_unsupport,Toast.LENGTH_LONG).show();
+			}
+		} catch (NumberFormatException e) {
+			Log.w(TAG, "Invalid display output format!");
+		}
+	}
 }
